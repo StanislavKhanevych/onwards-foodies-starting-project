7127b8519fb05dc36385db1a6a7efbc237c8ccd8
StanislavKhanevych <stanislav.anklav@gmail.com>
2024-06-11 16:37:44 +0200

    Added meal details

--- app/meals/[slug]/page.tsx
+++ app/meals/[slug]/page.tsx
@@ -1,8 +1,40 @@
-export default function MealItem({params}) {
-    return (
-        <main>
-            <h1>Meal Item</h1>
-            <h1>{params.slug}</h1>
-        </main>
-    )
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:EMAIL`}>NAME</a>
+          </p>
+          <p className={classes.summary}>SUMMARY</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <main className="not-found">
+      <h1>Page not found</h1>
+    </main>
+  );
+}
